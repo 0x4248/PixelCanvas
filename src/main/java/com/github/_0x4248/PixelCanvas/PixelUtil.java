7c3a2a360c3e307a633f2b219a18c41c3515124b
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-23 18:00:25 +0100

    Add line tools
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- src/main/java/com/github/_0x4248/PixelCanvas/PixelUtil.java
+++ src/main/java/com/github/_0x4248/PixelCanvas/PixelUtil.java
@@ -93,4 +93,54 @@ public class PixelUtil {
             outline(canvas, x, y, radius, outlineColor);
         }
     }
+
+    public static class line {
+        public static void draw(PixelCanvas canvas, int x1, int y1, int x2, int y2, Color color) {
+            int dx = Math.abs(x2 - x1);
+            int dy = Math.abs(y2 - y1);
+            int sx = x1 < x2 ? 1 : -1;
+            int sy = y1 < y2 ? 1 : -1;
+            int err = dx - dy;
+            while (true) {
+                canvas.setPixelWithoutUpdate(x1, y1, color);
+                if (x1 == x2 && y1 == y2) break;
+                int e2 = 2 * err;
+                if (e2 > -dy) {
+                    err -= dy;
+                    x1 += sx;
+                }
+                if (e2 < dx) {
+                    err += dx;
+                    y1 += sy;
+                }
+            }
+            canvas.repaint();
+        }
+
+        public static void dotted(PixelCanvas canvas, int x1, int y1, int x2, int y2, Color color) {
+            int dx = Math.abs(x2 - x1);
+            int dy = Math.abs(y2 - y1);
+            int sx = x1 < x2 ? 1 : -1;
+            int sy = y1 < y2 ? 1 : -1;
+            int err = dx - dy;
+            int count = 0;
+            while (true) {
+                if (count % 2 == 0) {
+                    canvas.setPixelWithoutUpdate(x1, y1, color);
+                }
+                if (x1 == x2 && y1 == y2) break;
+                int e2 = 2 * err;
+                if (e2 > -dy) {
+                    err -= dy;
+                    x1 += sx;
+                }
+                if (e2 < dx) {
+                    err += dx;
+                    y1 += sy;
+                }
+                count++;
+            }
+            canvas.repaint();
+        }
+    }
 }
